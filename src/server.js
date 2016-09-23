7979de88ef7d3a336ca8927d6d2648585b6fe73c
rekarnar <rekarnar@gmail.com>
2016-09-23 20:56:55 +0545

    server config

--- src/server.js
+++ src/server.js
@@ -61,6 +61,28 @@ let runServer = () => {
 				// require('chokidar-socket-emitter')({port: 8070, path: 'dist/client/scripts'});
 			// }
 
+			if (devMode) {
+				console.log('Loading CORS Headers...');
+				// Add CORS headers - DEV
+				app.use(function (req, res, next) {
+					// Website you wish to allow to connect
+					res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8090');
+
+					// Request methods you wish to allow
+					res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+					// Request headers you wish to allow
+					res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+					// Set to true if you need the website to include cookies in the requests sent
+					// to the API (e.g. in case you use sessions)
+					res.setHeader('Access-Control-Allow-Credentials', true);
+
+					// Pass to next layer of middleware
+					next();
+				});
+			}
+
 			itemEndpoints(app, pool, handleError)
 			tagEndpoints(app, pool, handleError)
 
